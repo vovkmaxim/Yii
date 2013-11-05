82fb7bd7c512dfcce7eeb2be2cbe62692f07f4ab
dmlyashko <dmlyashko@gmail.com>
2013-11-05 11:43:26 +0200

    Mobile big update

--- protected/config/main.php
+++ protected/config/main.php
@@ -48,26 +48,26 @@ return array(
             ),
         ),
 
-//        'db'=>array(
-//            'connectionString' => 'mysql:host=chdev.com.ua;dbname=chisw',
-//            'class' => 'CDbConnection',
-//            'emulatePrepare' => true,
-//            'username' => 'chisw',
-//            'password' => 't%f{.PdrUFn\rud',
-//            'charset' => 'utf8',
-//            'schemaCachingDuration' => 10
-//        ),
-
-
         'db'=>array(
-            'connectionString' => 'mysql:host=localhost;dbname=chi',
+            'connectionString' => 'mysql:host=chdev.com.ua;dbname=chisw',
             'class' => 'CDbConnection',
             'emulatePrepare' => true,
-            'username' => 'root',
-            'password' => '21091989',
+            'username' => 'chisw',
+            'password' => 't%f{.PdrUFn\rud',
             'charset' => 'utf8',
             'schemaCachingDuration' => 10
         ),
+
+//
+//        'db'=>array(
+//            'connectionString' => 'mysql:host=localhost;dbname=chi',
+//            'class' => 'CDbConnection',
+//            'emulatePrepare' => true,
+//            'username' => 'root',
+//            'password' => '21091989',
+//            'charset' => 'utf8',
+//            'schemaCachingDuration' => 10
+//        ),
 
         'errorHandler'=>array(
             // use 'site/error' action to display errors
