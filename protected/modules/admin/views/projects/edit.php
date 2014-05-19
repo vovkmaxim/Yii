d2ec126f6480f556b5a89318a1fe1034c3ed6fc7
konstantin.kostin <konstantin.kostin@chisw.us>
2014-05-19 11:58:44 +0300

    update views form

--- protected/modules/admin/views/projects/edit.php
+++ protected/modules/admin/views/projects/edit.php
@@ -1,72 +1,7 @@
 <div class="span4">
     <h2>Изменение проекта</h2>
-    <?php $form = $this->beginWidget('bootstrap.widgets.TbActiveForm'); ?>
-
-    <?php if($form->error($model, 'title')): ?>
-        <div class="alert alert-error">
-            <?php echo $form->error($model, 'title'); ?>
-        </div>
-    <?php endif; ?>
-
-    <?php if($form->error($model, 'file')): ?>
-        <div class="alert alert-error">
-            <?php echo $form->error($model, 'file'); ?>
-        </div>
-    <?php endif; ?>
-
-    <div class="control-group">
-      Технология
-      <div class="controls">
-        <?php echo $form->dropDownList($tech,'title', Tech::titleList(), array('options' => array($techId =>array('selected'=>true)))); ?>
-      </div>
-    </div>
-
-    <div class="control-group">
-        Теги - <a href="#" onclick="openTagsList();" id="clickTag">открыть</a>
-        <div class="controls">
-            <input type="text" value="<?php
-            foreach($tagsListProjects as $item){
-                echo $item['title'].', ';
-            }
-            ?>" name="Projects[tags]" class="input-xxlarge" id="Projects_tags" />
-        </div>
-    </div>
-
-    <div class="control-group" id="tagsList">
-        <?php
-        foreach($tagsList as $item){
-            echo '<a href="#" class="itemTag">'. $item->title .'</a>, ';
-        }
-        ?>
-    </div>
-
-    <div class="control-group">
-        <?php echo $form->label($model,'Название*', array('for' => 'Documents_title')); ?>
-        <div class="controls">
-            <?php echo $form->textField($model,'title', array('class' => 'input-xxlarge')); ?>
-        </div>
-    </div>
-
-    <div class="control-group">
-        <div class="controls">
-            <?php echo $form->ckEditorRow(
-                $model,
-                'description',
-                array(
-                    'editorOptions' => array(
-                        'fullpage' => 'js:true',
-                        'width' => '840',
-                        'resize_maxWidth' => '640',
-                        'resize_minWidth' => '320'
-                    )
-                )
-            ); ?>
-        </div>
-    </div>
-
-    <div class="controls">
-        <?php echo CHtml::submitButton('Сохранить', array('class' => 'btn btn-save')); ?>
-    </div>
-
-    <?php $this->endWidget(); ?>
-</div>
+    <?php $this->renderPartial('_form', array(
+        'model' => $model,
+        'tech' => $tech,
+        'tagsList' => $tagsList
+    )); ?>
