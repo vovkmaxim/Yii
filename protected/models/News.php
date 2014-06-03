f26a2517d14519f3596ea01619e92add8adbabb9
php <php@php-System-Product-Name>
2014-06-03 18:14:03 +0300

    News Model

---
+++ protected/models/News.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * This is the model class for table "news".
+ *
+ * The followings are the available columns in table 'news':
+ * @property integer $id
+ * @property integer $rubric_id
+ * @property string $title
+ * @property string $description
+ * @property string $author
+ *
+ * The followings are the available model relations:
+ * @property PromNewReview[] $promNewReviews
+ * @property PromTagNew[] $promTagNews
+ */
+class News extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'news';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('rubric_id, title, description, author', 'required'),
+			array('rubric_id', 'numerical', 'integerOnly'=>true),
+			array('title, author', 'length', 'max'=>255),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, rubric_id, title, description, author', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'promNewReviews' => array(self::HAS_MANY, 'PromNewReview', 'news_id'),
+			'promTagNews' => array(self::HAS_MANY, 'PromTagNew', 'news_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'rubric_id' => 'Rubric',
+			'title' => 'Title',
+			'description' => 'Description',
+			'author' => 'Author',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('rubric_id',$this->rubric_id);
+		$criteria->compare('title',$this->title,true);
+		$criteria->compare('description',$this->description,true);
+		$criteria->compare('author',$this->author,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return News the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
