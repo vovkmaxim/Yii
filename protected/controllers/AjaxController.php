5e3f097d3e518258f5c0a2f646b661791f911cdf
Maks Rudchenko <maksim.rudchenko@chisw.us>
2014-06-03 13:22:01 +0300

    zip

--- protected/controllers/AjaxController.php
+++ protected/controllers/AjaxController.php
@@ -2,38 +2,49 @@
 
 class AjaxController extends Controller
 {
-	public function actionDocuments()
-	{		
-		unset($_POST['email']);
-		$post_count = count($_POST);
-					
-		if($post_count > 1){				
-			if(is_file('documents/download/documents.tar')){
-				unlink('documents/download/documents.tar');
-			}		
-			
-			$filename = 'documents/download/documents.tar';
-			try {
-				$tarArchive = new PharData($filename);
-				
-				foreach($_POST as $item){
-					$tarArchive->addFile('documents/'.$item);
-				}
-				echo CJSON::encode(array('true'=> 'download/documents.tar'));
-			} catch (Exception $e) {
-				print "Возникло исключение: " . $e;
-			}
-		}else{
-			foreach($_POST as $item){
-				echo CJSON::encode(array('true'=> $item));
-			}
-		}	
-	}
+    public function actionDocuments()
+    {
+        $overwrite = false;
+        unset($_POST['email']);
+        $post_count = count($_POST);
+
+        if($post_count > 1){
+            $filename = 'documents/download/documents.zip';
+            if(file_exists($filename)&&!$overwrite) { unlink($filename); }
+
+            $files = array();
+
+            foreach($_POST as $item)
+            {
+                $files[] = $item;
+            }
+
+            $zip = new ZipArchive();
+
+            if($zip->open($filename,$overwrite ? ZIPARCHIVE::OVERWRITE : ZIPARCHIVE::CREATE) === true)
+            {
+                foreach($files as $file)
+                {
+                    $zip->addFile('documents/'.$file, $file);
+                }
+                $zip->close();
+                echo CJSON::encode(array('true'=> 'download/documents.zip'));
+
+            } else echo 'failed';
+        } else
+            {
+                foreach($_POST as $item)
+                {
+                    echo CJSON::encode(array('true'=> $item));
+                }
+            }
+    }
+
 	
 	public function actionDocumentsemail()
 	{		
 		$post_email = Yii::app()->request->getPost('email');
-		
+
 		if(empty($post_email)) {
 			echo CJSON::encode(array('error'=> 'Please enter your e-mail'));
 		}elseif(filter_var($post_email, FILTER_VALIDATE_EMAIL)) {
@@ -53,32 +64,41 @@ class AjaxController extends Controller
 
 			unset($_POST['email']);
 			$post_count = count($_POST);
-			
-			if($post_count > 1){		
-				if(file_exists('documents/download/documents.tar')){
-					unlink('documents/download/documents.tar');
-				}		
-				
-				$filename = 'documents/download/documents.tar';
-				try {
-					$tarArchive = new PharData($filename);
-					
-					foreach($_POST as $item){
-						$tarArchive->addFile('documents/'.$item);
-					}
-					
-					$mailer->setAttachment($filename);
-				} catch (Exception $e) {
-					print "Возникло исключение: " . $e;
-				}
-			}else{
-				foreach($_POST as $item){
-					$mailer->setAttachment('documents/'.$item);
-				}
-			}
-			
+
+            if($post_count > 1){
+                $filename = 'documents/download/documents.zip';
+                if(file_exists($filename)) { unlink($filename); }
+
+                $files = array();
+
+                foreach($_POST as $item)
+                {
+                    $files[] = $item;
+                }
+
+                $zip = new ZipArchive();
+
+                if($zip->open($filename,ZIPARCHIVE::CREATE) === true)
+                {
+                    foreach($files as $file)
+                    {
+                        $zip->addFile('documents/'.$file, $file);
+                    }
+                    $zip->close();
+                    $mailer->setAttachment($filename);
+
+                } else echo 'failed';
+            } else
+            {
+                foreach($_POST as $item)
+                {
+                    $mailer->setAttachment('documents/'.$item);
+                }
+            }
+
+
 			$mailer->send();
-			echo CJSON::encode(array('true'=> 'Thank you, the document has been sent to your e-mail ('. $post_email .').'));	
+			echo CJSON::encode(array('true'=> 'Thank you, checked documents has been sent to your e-mail ('. $post_email .').'));
 				
 		}else{
 			echo CJSON::encode(array('error'=> 'Your e-mail seems to be wrong. Please check it.'));
@@ -93,7 +113,7 @@ class AjaxController extends Controller
             echo CJSON::encode(array('error'=> 'Please enter your e-mail'));
         }elseif(filter_var($post_email, FILTER_VALIDATE_EMAIL)) {
             $mailer = new YiiMailer();
-            $mailer->setFrom('chisw_info@chisw.us', 'Summory');
+            $mailer->setFrom('chisw_info@chisw.us', 'Summary');
             $mailer->setSubject('CHI');
             $mailer->setView('cv');
             $mailer->setData(array('msg' => ''));
@@ -111,5 +131,68 @@ class AjaxController extends Controller
         }
     }
 }
+//	public function actionDocuments()
+//	{
+//		unset($_POST['email']);
+//		$post_count = count($_POST);
+//
+//		if($post_count > 1){
+//			if(is_file('documents/download/documents.zip')){
+//				unlink('documents/download/documents.zip');
+//			}
+//
+//			$filename = 'documents/download/documents.zip';
+//			try {
+//				$zipArchive = new ZipArchive($filename);
+//                ZIPARCHIVE::CREATE;
+//                if ($zipArchive->open($filename)) {
+//                    foreach($_POST as $item){
+//                        $zipArchive->addFile('documents/'.$item);
+//                    }
+//                    $zipArchive->close();
+//                }
+//
+//
+//				echo CJSON::encode(array('true'=> 'download/documents.zip'));
+//			} catch (Exception $e) {
+//				print "Exception: " . $e;
+//			}
+//		}else{
+//			foreach($_POST as $item){
+//				echo CJSON::encode(array('true'=> $item));
+//			}
+//		}
+//	}
 
-
+//unset($_POST['email']);
+//$post_count = count($_POST);
+//
+//if($post_count > 1){
+//    if(file_exists('documents/download/documents.zip')){
+//        unlink('documents/download/documents.zip');
+//    }
+//
+//    $filename = 'documents/download/documents.zip';
+//    try {
+//        $zipArchive = new ZipArchive($filename);
+//
+//        foreach($_POST as $item){
+//            $zipArchive->addFile('documents/'.$item);
+//        }
+//
+//        $mailer->setAttachment($filename);
+//    } catch (Exception $e) {
+//        print "Exception: " . $e;
+//    }
+//}else{
+//    foreach($_POST as $item){
+//        $mailer->setAttachment('documents/'.$item);
+//    }
+//}
+//
+//$mailer->send();
+//echo CJSON::encode(array('true'=> 'Thank you, the document has been sent to your e-mail ('. $post_email .').'));
+//
+//}else{
+//    echo CJSON::encode(array('error'=> 'Your e-mail seems to be wrong. Please check it.'));
+//}
