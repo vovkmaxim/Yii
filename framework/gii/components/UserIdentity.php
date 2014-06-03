97997c6933285d98e0dacbf5eb9f1eb820b779cd
php <php@php-System-Product-Name>
2014-06-03 18:40:29 +0300

    change

--- framework/gii/components/UserIdentity.php
+++ framework/gii/components/UserIdentity.php
@@ -1,20 +1,26 @@
 <?php
 
 class UserIdentity extends CUserIdentity
-{
-	/**
-	 * Authenticates a user.
-	 * @return boolean whether authentication succeeds.
-	 */
-	public function authenticate()
-	{
-		$password=Yii::app()->getController()->getModule()->password;
-		if($password===null)
-			throw new CException('Please configure the "password" property of the "gii" module.');
-		elseif($password===false || $password===$this->password)
-			$this->errorCode=self::ERROR_NONE;
+{ 
+    private $_id;
+    public function authenticate()
+    {
+        $record = Apiusers::model()->findByAttributes(array('username'=>$this->username));
+        if($record===null)
+            $this->errorCode=self::ERROR_USERNAME_INVALID;
+        else if($record->password!==crypt($this->password))
+            $this->errorCode=self::ERROR_PASSWORD_INVALID;
         else
-            $this->errorCode=self::ERROR_UNKNOWN_IDENTITY;
-		return !$this->errorCode;
-	}
+        {
+            $this->_id=$record->id;
+            $this->setState('title', $record->title);
+            $this->errorCode=self::ERROR_NONE;
+        }
+        return !$this->errorCode;
+    }
+ 
+    public function getId()
+    {
+        return $this->_id;
+    }
 }
