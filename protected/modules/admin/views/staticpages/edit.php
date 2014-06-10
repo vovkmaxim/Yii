f22fa17e8e83dcc01d9c9aa5951eedcf57eb9ec3
php <php@php-System-Product-Name>
2014-06-10 13:08:52 +0300

    add flag for javascript

--- protected/modules/admin/views/staticpages/edit.php
+++ protected/modules/admin/views/staticpages/edit.php
@@ -1,5 +1,10 @@
 <a href="<?php echo Yii::app()->request->baseUrl; ?>/admin/staticpages">&lt;&nbsp;&lt;&nbsp;&lt;&nbsp; Back </a>
 <h2>Update static content for  "<?php echo $model->title; ?>"</h2>
-<?php $this->renderPartial('form', array('model' => $model)); ?>
+<?php 
+       $render = array('model' => $model);
+        if(isset($flag)){
+            $render['flag'] = $flag;
+        }
+$this->renderPartial('form', $render); ?>
 
 
